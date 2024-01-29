a501caf4ed06a66b94c7ca139d697866702bcbe1
Amaan <amaanzaidi462@gmail.com>
2024-01-30 05:25:22 +0530

    Second-Commit

--- redux-blog/src/App.js
+++ redux-blog/src/App.js
@@ -1,5 +1,13 @@
+import PostList from "./features/posts/PostList";
+import AddPostForm from "./features/posts/AddPostForm";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <main classname="App">
+      <AddPostForm />
+      <PostList />
+    </main>
+  );
 }
 
 export default App;
--- redux-blog/src/index.js
+++ redux-blog/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import store from "./app/store";
+import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+store.dispatch(fetchUsers());
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
